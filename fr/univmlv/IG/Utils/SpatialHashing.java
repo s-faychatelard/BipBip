b10941ee6995fc01a592562df9a8f650f47d1a49
djubeau <djubeau@djubeau-Vostro-V131>
2012-05-22 19:57:08 +0200

    YOUHOUUUUU

--- fr/univmlv/IG/Utils/SpatialHashing.java
+++ fr/univmlv/IG/Utils/SpatialHashing.java
@@ -7,41 +7,56 @@ public class SpatialHashing {
 
 	
 	public static void main(String[] args) {
+		
+		//assert (compute(14.2f, 13.3f).equals(compute(14.3f, 13.3f)));
+		
+		
+		System.out.println(compute(-50f, 20f));
+
 		System.out.println(compute(14.2f, 13.3f));
-		System.out.println(compute(-14.2f, 13.3f));
+		System.out.println(compute(14.2001f, 13.3f));
+
+//		System.out.println(compute(30f, 13.3f));
+//
+//		System.out.println(compute(-14.2f, -13.3f));
+//		System.out.println(compute(0f, 0f));
+
 
 	}
 	
 	public static String compute(float lat, float lng) {
 		
-		return computeRec(lat+90, lng+180, new StringBuffer(), 2);
+		return computeRec(lat+90, lng+180, MAX_LAT, MAX_LNG, new StringBuffer(), 1);
 	}
 	
-	private static String computeRec(float lat, float lng, StringBuffer sb, int level) {
+	private static String computeRec(float lat, float lng, double compLat, double compLng, StringBuffer sb, int level) {
+
+		if (level >= 25)
+			return sb.toString();
 		
-		if(lat > MAX_LAT/level) { // NORTH
-			if(lng < MAX_LNG/level) { // WEST
+		if(lat < compLat) { // NORTH
+			if(lng < compLng) { // WEST
 				sb.append('0');
+				return computeRec(lat,  lng, compLat-MAX_LAT/Math.pow(2,level), compLng-MAX_LNG/Math.pow(2,level),  sb,  level+1);
 			}
 			else { // EAST
 				sb.append('1');
+				return computeRec(lat,  lng, compLat-MAX_LAT/Math.pow(2,level), compLng+MAX_LNG/Math.pow(2,level),  sb,  level+1);
+
 			}
 			
 		}
 		
 		else {  // SOUTH
-			if(lng < MAX_LNG/level) { // WEST
+			if(lng < compLng) { // WEST
 				sb.append('2');
+				return computeRec(lat,  lng, compLat+MAX_LAT/Math.pow(2,level), compLng-MAX_LNG/Math.pow(2,level),  sb,  level+1);
 			}
 			else { // EAST
 				sb.append('3');
+				return computeRec(lat,  lng, compLat+MAX_LAT/Math.pow(2,level), compLng+MAX_LNG/Math.pow(2,level),  sb,  level+1);
 			}
 		}
-		
-		if (level >= Math.pow(2, 15))
-			return sb.toString();
-		
-		return computeRec(lat,  lng,  sb,  level*2);
 	}
 
 }
